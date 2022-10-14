a7e46de613e0231b275b5f084f85815d5cd4b087
MOSAMA375 <107075468+MOSAMA375@users.noreply.github.com>
2022-10-14 13:09:09 -0400

    Update lines.py

--- lines.py
+++ lines.py
@@ -3,25 +3,26 @@
 import csv
 
 
-num=200
-abs_value=3
-outputlines = f'file_output_lines'
-file = f'outdata{num}'
+num=60
+abs_value=-162000
+outputlines = f'1file_output_lines'
+file = f'out_data{num}'
 i=0
 
 
 with open(outputlines,'w') as wr:
     with open(file,'r') as csvfile:
         csvreader = csv.reader(csvfile, delimiter=" ")
-        conv_abs_value=str(abs_value)
+        conv_abs_value1=abs_value/1000000
+        conv_abs_value=float(conv_abs_value1)
         for row in csvreader:
             i+=1
-            data_point=row[1]
+            data_point=float(row[1])
             conv_i=str(i)
-            if data_point < conv_abs_value:
+            if data_point >= conv_abs_value:
                 wr.write(f'NR=={conv_i} || ')
             else:
                 pass
-            
+
 
 
