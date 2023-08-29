544640d68f4bfadbe987928892e83cfbfc95114e
MOSAMA375 <107075468+MOSAMA375@users.noreply.github.com>
2023-08-29 15:44:38 -0400

    Create mutiple_process.sh
    It will launch the jet tangent for all time steps independently.

---
+++ mutiple_process.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for ((i = 0; i <= 1; i++)); do
+  for ((j = 0; j <= 9; j++)); do
+    for ((k = 0; k <= 9; k++)); do
+      for ((l = 0; l <= 9; l++)); do
+        t=${i}.${j}${k}${l}
+        FILE=i-${i}.${j}${k}${l}.gnu
+        if [ -f "$FILE" ]; then
+          mkdir Jet$t
+          cp i-$t.gnu Jet$t/i-$t.gnu
+          cp jet_cut.sh Jet$t/jet_cut.sh
+          chmod +x Jet$t/jet_cut.sh
+          cd Jet$t
+          nohup ./jet_cut.sh &
+          
+        fi
+      done
+    done
+  done
+done
