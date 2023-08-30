7f07b5119b8a2b7f172c3dc672bc6b18cfaf1459
MOSAMA375 <107075468+MOSAMA375@users.noreply.github.com>
2023-08-30 14:29:37 -0400

    Update mutiple_process.sh

--- mutiple_process.sh
+++ mutiple_process.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for ((i = 0; i <= 1; i++)); do
-  for ((j = 0; j <= 9; j++)); do
-    for ((k = 0; k <= 9; k++)); do
-      for ((l = 0; l <= 9; l++)); do
+for ((i = 0; i < 1; i++)); do
+  for ((j = 0; j <= 1; j++)); do
+    for ((k = 0; k <= 9; k+=2)); do
+      for ((l = 0; l < 1; l++)); do
         t=${i}.${j}${k}${l}
         FILE=i-${i}.${j}${k}${l}.gnu
         if [ -f "$FILE" ]; then
@@ -11,9 +11,10 @@ for ((i = 0; i <= 1; i++)); do
           cp i-$t.gnu Jet$t/i-$t.gnu
           cp jet_cut.sh Jet$t/jet_cut.sh
           chmod +x Jet$t/jet_cut.sh
-          cd Jet$t
-          nohup ./jet_cut.sh &
-          
+          (
+            cd Jet$t
+            nohup ./jet_cut.sh &
+          )
         fi
       done
     done
